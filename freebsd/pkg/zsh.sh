41912101744ea232bccdcf0786c9de9969138923
Neo Chan <netkiller@msn.com>
2013-08-23 19:29:49 +0800

    Update zsh.sh

--- freebsd/pkg/zsh.sh
+++ freebsd/pkg/zsh.sh
@@ -3,11 +3,18 @@ chsh -s zsh
 # usermod -s /usr/local/bin/zsh neo
 # chpass -s /usr/local/bin/zsh
 
-cat > ~/.zshrc <<EOD
+cat > ~/.zshrc <<'EOD'
 # Created by neo <netkiller@msn.com>
 PROMPT='%n@%M:%~$ '
 
+HISTFILE=~/.history
+HISTSIZE=1000
+SAVEHIST=1000
+
 # enable color support of ls and also add handy aliases
+autoload colors; colors
+export PS1="%B[%{$fg[red]%}%n%{$reset_color%}%b@%B%{$fg[cyan]%}%m%b%{$reset_color%}:%~%B]%b "
+
 if [ -x /usr/bin/dircolors ]; then
     eval "`dircolors -b`"
     alias ls='ls --color=auto'
@@ -29,7 +36,13 @@ bindkey "\e[1~" beginning-of-line
 bindkey "\e[2~" overwrite-mode
 bindkey "\e[3~" delete-char
 bindkey "\e[4~" end-of-line
+bindkey "\e[5~" beginning-of-history
+bindkey "\e[6~" end-of-history
 bindkey "\e[7~" beginning-of-line
 bindkey "\e[8~" end-of-line
+
+if [ -r ${HOME}/.profile ]; then
+	. ${HOME}/.profile
+fi
 
 EOD
