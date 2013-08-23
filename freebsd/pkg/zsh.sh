8e148bb56f563d3e9f2c471038b301e306b59d15
Neo Chan <netkiller@msn.com>
2013-08-23 20:37:49 +0800

    Update zsh.sh

--- freebsd/pkg/zsh.sh
+++ freebsd/pkg/zsh.sh
@@ -5,15 +5,20 @@ chsh -s zsh
 
 cat > ~/.zshrc <<'EOD'
 # Created by neo <netkiller@msn.com>
-PROMPT='%n@%M:%~$ '
-
-HISTFILE=~/.history
-HISTSIZE=1000
-SAVEHIST=1000
+zstyle ':completion:*' menu select
+setopt completealiases
 
 # enable color support of ls and also add handy aliases
-autoload colors; colors
-export PS1="%B[%{$fg[red]%}%n%{$reset_color%}%b@%B%{$fg[cyan]%}%m%b%{$reset_color%}:%~%B]%b "
+# autoload colors; colors
+autoload -U colors compinit promptinit
+colors
+compinit
+promptinit
+
+PROMPT='%n@%M:%~%# '
+#PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%1~ %{$reset_color%}%#"
+RPROMPT="[%{$fg[yellow]%}%?%{$reset_color%}]"
+PS1="%B[%{$fg[red]%}%n%{$reset_color%}%b@%B%{$fg[cyan]%}%m%b%{$reset_color%}:%~%B]%b%# "
 
 if [ -x /usr/bin/dircolors ]; then
     eval "`dircolors -b`"
@@ -24,12 +29,42 @@ if [ -x /usr/bin/dircolors ]; then
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
+    
+	# some more ls aliases
+	alias ll='ls -l'
+	alias la='ls -A'
+	alias l='ls -CF'    
 fi
 
-# some more ls aliases
-alias ll='ls -l'
-alias la='ls -A'
-alias l='ls -CF'
+# global aliases
+alias -g '...'='../..'
+alias -g '....'='../../..'
+
+if [[ $OSTYPE = freebsd* ]]; then 
+    export CLICOLOR="YES"
+    export LSCOLORS="ExGxFxdxCxDxDxhbadExEx"
+    export GREP_OPTIONS="--color=auto"
+    alias ls="ls -GF"
+    alias ll="ls -lhGF"
+    alias la="ls -lhaGF"
+fi
+
+# exporting colors
+export GREP_COLOR=31
+
+# even more colour-candy with app-misc/grc (http://goo.gl/2z2j)
+if [ "$TERM" != dumb ] && [ -x /usr/bin/grc ] ; then
+   alias cl='/usr/bin/grc -es --colour=auto'
+   alias configure='cl ./configure'
+   alias diff='cl diff'
+   alias gcc='cl gcc'
+   alias g++='cl g++'
+   alias as='cl as'
+   alias gas='cl gas'
+   alias ld='cl ld'
+   alias netstat='cl netstat'
+   alias ping='cl ping'
+fi
 
 # Home/End/Del key
 bindkey "\e[1~" beginning-of-line
@@ -41,8 +76,15 @@ bindkey "\e[6~" end-of-history
 bindkey "\e[7~" beginning-of-line
 bindkey "\e[8~" end-of-line
 
+HISTFILE=~/.history
+HISTSIZE=1000
+SAVEHIST=1000
+
 if [ -r ${HOME}/.profile ]; then
 	. ${HOME}/.profile
+fi
+if [ -r ${HOME}/.login ]; then
+	. ${HOME}/.login
 fi
 
 EOD
